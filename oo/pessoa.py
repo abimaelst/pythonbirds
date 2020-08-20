006dac2d0e3eed2e82dfa2f21999df66eace766d
abimaelst <abimaelst@live.com>
2020-08-19 22:57:17 -0300

    metodos complexos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,19 @@
 class Pessoa:
-    def __init__(self):
-        self.nome = None
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(id(p))
-    print(Pessoa.cumprimentar(p))
-    print(p.nome)
-    p.nome = 'Abimael'
-    print(p.nome)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
